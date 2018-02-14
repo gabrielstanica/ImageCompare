d2732b31f6a5d8b33a8fd90abc4855e11def4f3c
gabrielstanica <gabriel.stanica@hotmail.com>
2018-02-14 09:43:27 +0200

    Update ImageProcess.cs

--- ImageCompare/ImageProcess.cs
+++ ImageCompare/ImageProcess.cs
@@ -60,36 +60,43 @@ namespace ImageCompare
         public bool CompareTwoImages(ImageProperties firstImage, ImageProperties secondImage)
         {
 
-            if (firstImage.Size.Equals(secondImage.Size) && firstImage.Resolution.Equals(secondImage.Resolution))
+            try
             {
-                var first = new ImageProperties(firstImage);
-                var second = new ImageProperties(secondImage);
-                if (first.Base64.Equals(second.Base64))
+                if (firstImage.Size.Equals(secondImage.Size) && firstImage.Resolution.Equals(secondImage.Resolution))
                 {
-                    Console.WriteLine("----------------------");
-                    Console.WriteLine("Image equals by base64");
-                    Console.WriteLine(firstImage.ImagePath);
-                    Console.WriteLine(secondImage.ImagePath);
-                    Console.WriteLine("----------------------");
-
-                    if (!new DirectoryInfo(CopyImagePath).Exists)
+                    var first = new ImageProperties(firstImage);
+                    var second = new ImageProperties(secondImage);
+                    if (first.Base64.Equals(second.Base64))
                     {
-                        Directory.CreateDirectory(CopyImagePath);
+                        Console.WriteLine("----------------------");
+                        Console.WriteLine("Image equals by base64");
+                        Console.WriteLine(firstImage.ImagePath);
+                        Console.WriteLine(secondImage.ImagePath);
+                        Console.WriteLine("----------------------");
+
+                        if (!new DirectoryInfo(CopyImagePath).Exists)
+                        {
+                            Directory.CreateDirectory(CopyImagePath);
+                        }
+
+                        Console.WriteLine(String.Format("Moved image {0}", firstImage.ImagePath));
+                        Console.WriteLine(String.Format("Moved image {0}", secondImage.ImagePath));
+                        Console.WriteLine("----------------------");
+                        var currentDate = DateTime.Now;
+                        var firstImageName = String.Format("{0}_{1}{2}{3}{4}", firstImage.ImageName, currentDate.Hour, currentDate.Minute, currentDate.Second, currentDate.Millisecond);
+                        var secondImageName = String.Format("{0}_{1}{2}{3}{4}Copy", secondImage.ImageName, currentDate.Hour, currentDate.Minute, currentDate.Second, currentDate.Millisecond);
+
+                        File.Move(firstImage.ImagePath, Path.Combine(CopyImagePath, firstImageName));
+                        File.Copy(secondImage.ImagePath, Path.Combine(CopyImagePath, secondImageName));
+                        //File.Delete(firstImage.ImagePath);
+                        firstImage.ToDelete = true;
+                        return true;
                     }
-
-                    Console.WriteLine(String.Format("Moved image {0}", firstImage.ImagePath));
-                    Console.WriteLine(String.Format("Moved image {0}", secondImage.ImagePath));
-                    Console.WriteLine("----------------------");
-                    var currentDate = DateTime.Now;
-                    var firstImageName = String.Format("{0}_{1}{2}{3}{4}", firstImage.ImageName, currentDate.Hour, currentDate.Minute, currentDate.Second, currentDate.Millisecond);
-                    var secondImageName = String.Format("{0}_{1}{2}{3}{4}Copy", secondImage.ImageName, currentDate.Hour, currentDate.Minute, currentDate.Second, currentDate.Millisecond);
-
-                    File.Move(firstImage.ImagePath, Path.Combine(CopyImagePath, firstImageName));
-                    File.Copy(secondImage.ImagePath, Path.Combine(CopyImagePath, secondImageName));
-                    //File.Delete(firstImage.ImagePath);
-                    firstImage.ToDelete = true;
-                    return true;
                 }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
             }
             return false;
         }
