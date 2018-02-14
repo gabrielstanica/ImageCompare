a4e5309f9de429523bc962f1a7aa5852d5151e84
gabrielstanica <gabriel.stanica@hotmail.com>
2018-02-14 09:42:34 +0200

    Update ImageActions.cs

--- ImageCompare/ImageActions.cs
+++ ImageCompare/ImageActions.cs
@@ -1,4 +1,4 @@
-﻿using ImageCompare.Interface;
+using ImageCompare.Interface;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
@@ -77,28 +77,38 @@ namespace ImageCompare
         }
         public void CreateDirectoriesMoveFiles(List<ImageProperties> list)
         {
-            foreach(var item in list)
+            foreach (var item in list)
             {
                 MoveFile(item);
             }
         }
         private void MoveFile(ImageProperties image)
         {
-            // https://csharp.today/log4net-tutorial-great-library-for-logging/
-            var directoryDate = String.Format("{0}.{1}", image.CreationDate.Year,image.CreationDate.Month);
-            var newImagesPath = Path.Combine(NewPath, directoryDate);
-            if(!Directory.Exists(newImagesPath))
+            try
             {
-                Directory.CreateDirectory(newImagesPath);
+                if (!image.ToDelete)
+                {
+                    // https://csharp.today/log4net-tutorial-great-library-for-logging/
+                    var directoryDate = String.Format("{0}.{1}", image.CreationDate.Year, image.CreationDate.Month);
+                    var newImagesPath = Path.Combine(NewPath, directoryDate);
+                    if (!Directory.Exists(newImagesPath))
+                    {
+                        Directory.CreateDirectory(newImagesPath);
+                    }
+                    Console.Write(String.Format("\rMoving image {0}", image.ImagePath));
+                    var fileName = Path.Combine(newImagesPath, image.ImageName);
+                    var currentDate = DateTime.Now;
+                    if (File.Exists(fileName))
+                    {
+                        fileName = fileName.Replace(image.ImageName, String.Format("{0}{1}{2}", image.ImageName, currentDate.Minute, currentDate.Millisecond));
+                    }
+                    File.Move(image.ImagePath, Path.Combine(newImagesPath, fileName));
+                }
             }
-            Console.Write(String.Format("\rMoving image {0}", image.ImagePath));
-            var fileName = Path.Combine(newImagesPath, image.ImageName);
-            var currentDate = DateTime.Now;
-            if (File.Exists(fileName))
+            catch(Exception ex)
             {
-                fileName.Replace(image.ImageName, String.Format("{0}{1}{2}", image.ImageName,currentDate.Minute,currentDate.Millisecond));
+                Console.WriteLine(ex.Message);
             }
-            File.Move(image.ImagePath, Path.Combine(newImagesPath, fileName));
         }
 
     }
